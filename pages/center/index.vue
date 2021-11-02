ce59bb79f5925dfcadfc5f8596205bd5f33a8d6e
handsomeshuaifan <642746667@qq.com>
2021-11-02 14:54:57 +0800

    first commit

---
+++ pages/center/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		关于
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
